5a2017a92fdbde15a4c999df1b85987f5b1b9154
James He <jameshe2018@u.northwestern.edu>
2016-11-20 07:46:06 +0000

    licence = phone number hash finished

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -5,7 +5,8 @@ class SendTextController < ApplicationController
   def send_text_message
     number_to_send_to = params[:session][:license]
     msgc = params[:session][:content]
-
+    u = User.find_by_licensep(number_to_send_to)
+    phone = u[:phonen]
     twilio_sid = "AC62e2fbdd4e525040a2ba75be63acc904"
     twilio_token = "e05696dc4e52734fc388bd05f018c3c3"
     twilio_phone_number = "3317029479"
@@ -14,7 +15,7 @@ class SendTextController < ApplicationController
 
     @twilio_client.account.sms.messages.create(
       :from => "+1#{twilio_phone_number}",
-      :to => "#{number_to_send_to}",
+      :to => "+1#{phone}",
       :body => "#{msgc}"
     )
   end
